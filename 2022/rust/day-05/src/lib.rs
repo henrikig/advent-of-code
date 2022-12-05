d8c4df01888cc2ff8a6e10492ec8b1604d7c1996
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2022-12-05 22:01:18 +0100

    :sparkles: completed day 5

--- 2022/rust/day-05/src/lib.rs
+++ 2022/rust/day-05/src/lib.rs
@@ -1,21 +1,28 @@
 use regex::Regex;
 
 pub fn process_part1(input: &str) -> String {
+    // Separate crates from moves
     let (initial_order, operations) = input.split_once("\n\n").unwrap();
     let mut initial_order: Vec<&str> = initial_order.split("\n").collect();
     let operations: Vec<&str> = operations.split("\n").collect();
 
-    let num = initial_order.pop().unwrap();
-    let num: Vec<_> = num.split(" ").filter(|&entry| !entry.is_empty()).collect();
-    let width = num.len();
+    // Get the number of horizontal stacks
+    let num_stacks = initial_order.pop().unwrap();
+    let num_stacks: Vec<_> = num_stacks
+        .split(" ")
+        .filter(|&entry| !entry.is_empty())
+        .collect();
+    let num_stacks = num_stacks.len();
 
+    // Regex to match on `[<character>]`
     let re = Regex::new(r"\[(\w)\]").unwrap();
 
-    let mut stacks: Vec<Vec<String>> = Vec::with_capacity(width);
-    for _ in 0..width {
+    let mut stacks: Vec<Vec<String>> = Vec::with_capacity(num_stacks);
+    for _ in 0..num_stacks {
         stacks.push(Vec::new());
     }
 
+    // Match on crates and push them to stacks
     for &line in initial_order.iter().rev() {
         for mat in re.find_iter(line) {
             let col = mat.start() / 4;
@@ -23,10 +30,12 @@ pub fn process_part1(input: &str) -> String {
         }
     }
 
+    // Parse the operations and perform them
     let re = Regex::new(r"(\d+)").unwrap();
     for &operation in operations.iter() {
+        // ops = [num, from, to]
         let mut ops: Vec<usize> = Vec::with_capacity(3);
-        
+
         for mat in re.find_iter(operation) {
             let num_parsed = operation[mat.start()..mat.end()].parse::<usize>().unwrap();
             ops.push(num_parsed);
@@ -43,9 +52,11 @@ pub fn process_part1(input: &str) -> String {
 
     stacks
         .iter()
-        .map(|stack| stack.last().unwrap().to_string())
-        .collect::<Vec<String>>()
-        .join("")
+        .map(|stack| match stack.iter().last() {
+            Some(s) => s,
+            None => "",
+        })
+        .collect::<String>()
 }
 
 #[cfg(test)]
